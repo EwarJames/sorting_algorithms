1d3694f5e19126eaa32dcc30169b38b614f97c1f
EwarJames <ewaarjames@gmail.com>
2021-02-03 15:28:03 +0000

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,3 +16,34 @@ void _swapint(int *num1, int *num2)
 	*num1 = *num2;
 	*num2 = temp;
 }
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order.
+ *@array: Pointer to the elements
+ *@size: size of the array
+ *
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool s = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (s == false)
+	{
+		s = true;
+
+		for (i = 0; i < len - 1; i++)
+			if (array[i] > array[i + 1])
+			{
+				_swapint(array + i, array + i + 1);
+				print_array(array, size);
+				s = false
+			}
+	}
+	len--;
+}
